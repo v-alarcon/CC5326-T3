8eaeae83280f73b227f5d08c8860bf86ce8c48ab
vicente <vicente.alarcon@ug.uchile.cl>
2023-12-05 23:11:56 -0300

    protocol 100 half done

--- codigo_esp/main/t3.c
+++ codigo_esp/main/t3.c
@@ -6,6 +6,107 @@
 #include "esp_system.h"
 #include "nvs_flash.h"
 #include "nvs.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "esp_system.h"
+#include "esp_wifi.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/event_groups.h"
+#include "lwip/err.h"
+#include "lwip/sys.h"
+#include "nvs_flash.h"
+#include "lwip/sockets.h" // Para sockets
+#include "time.h" // Para obtener el tiempo
+#include "esp_sntp.h"
+#include "math.h"
+
+//Credenciales de WiFi
+#define WIFI_SSID "grupo2"
+#define WIFI_PASSWORD "g2passg2"
+#define SERVER_IP     "192.168.4.1" // IP del servidor
+#define SERVER_PORT   1234
+
+// Variables de WiFi
+#define WIFI_CONNECTED_BIT BIT0
+#define WIFI_FAIL_BIT BIT1
+static const char* TAG = "WIFI";
+static int s_retry_num = 0;
+static EventGroupHandle_t s_wifi_event_group;
+
+void event_handler(void* arg, esp_event_base_t event_base,
+                          int32_t event_id, void* event_data) {
+    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
+        esp_wifi_connect();
+    } else if (event_base == WIFI_EVENT &&
+               event_id == WIFI_EVENT_STA_DISCONNECTED) {
+        if (s_retry_num < 10) {
+            esp_wifi_connect();
+            s_retry_num++;
+            ESP_LOGI(TAG, "retry to connect to the AP");
+        } else {
+            xEventGroupSetBits(s_wifi_event_group, WIFI_FAIL_BIT);
+        }
+        ESP_LOGI(TAG, "connect to the AP fail");
+    } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
+        ip_event_got_ip_t* event = (ip_event_got_ip_t*)event_data;
+        ESP_LOGI(TAG, "got ip:" IPSTR, IP2STR(&event->ip_info.ip));
+        s_retry_num = 0;
+        xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
+    }
+}
+void wifi_init_sta(char* ssid, char* password) {
+    s_wifi_event_group = xEventGroupCreate();
+
+    ESP_ERROR_CHECK(esp_netif_init());
+
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    esp_netif_create_default_wifi_sta();
+
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+    esp_event_handler_instance_t instance_any_id;
+    esp_event_handler_instance_t instance_got_ip;
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(
+        WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler, NULL, &instance_any_id));
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(
+        IP_EVENT, IP_EVENT_STA_GOT_IP, &event_handler, NULL, &instance_got_ip));
+
+    wifi_config_t wifi_config;
+    memset(&wifi_config, 0, sizeof(wifi_config_t));
+
+    // Set the specific fields
+    strcpy((char*)wifi_config.sta.ssid, WIFI_SSID);
+    strcpy((char*)wifi_config.sta.password, WIFI_PASSWORD);
+    wifi_config.sta.threshold.authmode = WIFI_AUTH_WPA2_PSK;
+    wifi_config.sta.pmf_cfg.capable = true;
+    wifi_config.sta.pmf_cfg.required = false;
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
+    ESP_ERROR_CHECK(esp_wifi_start());
+
+    ESP_LOGI(TAG, "wifi_init_sta finished.");
+
+    EventBits_t bits = xEventGroupWaitBits(s_wifi_event_group,
+                                           WIFI_CONNECTED_BIT | WIFI_FAIL_BIT,
+                                           pdFALSE, pdFALSE, portMAX_DELAY);
+
+    if (bits & WIFI_CONNECTED_BIT) {
+        ESP_LOGI(TAG, "connected to ap SSID:%s password:%s", ssid,
+                 password);
+    } else if (bits & WIFI_FAIL_BIT) {
+        ESP_LOGI(TAG, "Failed to connect to SSID:%s, password:%s", ssid,
+                 password);
+    } else {
+        ESP_LOGE(TAG, "UNEXPECTED EVENT");
+    }
+
+    ESP_ERROR_CHECK(esp_event_handler_instance_unregister(
+        IP_EVENT, IP_EVENT_STA_GOT_IP, instance_got_ip));
+    ESP_ERROR_CHECK(esp_event_handler_instance_unregister(
+        WIFI_EVENT, ESP_EVENT_ANY_ID, instance_any_id));
+    vEventGroupDelete(s_wifi_event_group);
+}
 
 void app_main(void)
 {
@@ -30,13 +131,13 @@ void app_main(void)
         printf("Done\n");
 
         // Read
-        printf("Reading restart counter from NVS ... ");
-        int32_t restart_counter = 0; // value will default to 0, if not set yet in NVS
-        err = nvs_get_i32(my_handle, "restart_counter", &restart_counter);
+        printf("Reading config counter from NVS ... ");
+        int32_t config = 0; // value will default to 0, if not set yet in NVS
+        err = nvs_get_i32(my_handle, "config", &config);
+        printf("Committing updates in NVS ... ");
         switch (err) {
             case ESP_OK:
-                printf("Done\n");
-                printf("Restart counter = %" PRIu32 "\n", restart_counter);
+                printf("config = %" PRIu32 "\n", config);
                 break;
             case ESP_ERR_NVS_NOT_FOUND:
                 printf("The value is not initialized yet!\n");
@@ -45,32 +146,77 @@ void app_main(void)
                 printf("Error (%s) reading!\n", esp_err_to_name(err));
         }
 
-        // Write
-        printf("Updating restart counter in NVS ... ");
-        restart_counter++;
-        err = nvs_set_i32(my_handle, "restart_counter", restart_counter);
-        printf((err != ESP_OK) ? "Failed!\n" : "Done\n");
+        // case config = 0 esp ready to recibe config
+        if (config == 0)
+        {
+            wifi_init_sta(WIFI_SSID, WIFI_PASSWORD);
+            ESP_LOGI(TAG,"Conectado a WiFi!\n");
 
-        // Commit written value.
-        // After setting any values, nvs_commit() must be called to ensure changes are written
-        // to flash storage. Implementations may write to storage at other times,
-        // but this is not guaranteed.
-        printf("Committing updates in NVS ... ");
-        err = nvs_commit(my_handle);
-        printf((err != ESP_OK) ? "Failed!\n" : "Done\n");
+            struct sockaddr_in server_addr;
+            server_addr.sin_family = AF_INET;
+            server_addr.sin_port = htons(SERVER_PORT);
+            inet_pton(AF_INET, SERVER_IP, &server_addr.sin_addr.s_addr);
 
-        // Close
-        nvs_close(my_handle);
+            // Crear un socket
+            int sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+            if (sock < 0) {
+                ESP_LOGE(TAG, "Error al crear el socket");
+            }
+
+            // Conectar al servidor
+            if (connect(sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) != 0) {
+                ESP_LOGE(TAG, "Error al conectar");
+                close(sock);
+            }
+            char rx_buffer[128];
+            int rx_len = recv(sock, rx_buffer, sizeof(rx_buffer) - 1, 0);
+            if (rx_len < 0) {
+                ESP_LOGE(TAG, "Error al recibir datos");
+            }
+            rx_buffer[rx_len] = '\0'; // Para que no haya basura al final
+            ESP_LOGI(TAG, "Datos recibidos: %s", rx_buffer);
+            close(sock);
+            if (rx_buffer[0]=='1' && rx_buffer[1]=='0' && rx_buffer[2]=='0'){
+                ESP_LOGI(TAG , "Configuración correcta\n");
+                err = nvs_set_i32(my_handle, "config", 1);
+                printf((err != ESP_OK) ? "Failed!\n" : "Done\n");
+                printf("Committing updates in NVS ... ");
+                err = nvs_commit(my_handle);
+                printf((err != ESP_OK) ? "Failed!\n" : "Done\n");
+                nvs_close(my_handle);
+                esp_restart();
+            }
+        }
+
+        if (config == 1){
+            printf("Do protocol 0 ");
+            // set config to 0
+            err = nvs_set_i32(my_handle, "config", 0);
+            printf((err != ESP_OK) ? "Failed!\n" : "Done\n");
+            printf("Committing updates in NVS ... ");
+            err = nvs_commit(my_handle);
+            printf((err != ESP_OK) ? "Failed!\n" : "Done\n");
+            nvs_close(my_handle);
+        }
+        
+
+        // // Write
+        // printf("Updating restart counter in NVS ... ");
+        // restart_counter++;
+        // err = nvs_set_i32(my_handle, "restart_counter", restart_counter);
+        // printf((err != ESP_OK) ? "Failed!\n" : "Done\n");
+
+        // // Commit written value.
+        // // After setting any values, nvs_commit() must be called to ensure changes are written
+        // // to flash storage. Implementations may write to storage at other times,
+        // // but this is not guaranteed.
+        // printf("Committing updates in NVS ... ");
+        // err = nvs_commit(my_handle);
+        // printf((err != ESP_OK) ? "Failed!\n" : "Done\n");
+
+        // // Close
+        // nvs_close(my_handle);
     }
-
-    printf("\n");
-
-    // Restart module
-    for (int i = 10; i >= 0; i--) {
-        printf("Restarting in %d seconds...\n", i);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-    }
-    printf("Restarting now.\n");
-    fflush(stdout);
-    esp_restart();
+    // fflush(stdout);
+    // esp_restart();
 }
--- codigo_rasp/server.py
+++ codigo_rasp/server.py
@@ -1,4 +1,6 @@
 from modelos import *
+import socket
+import datetime
 
 # Datos WIFI
 HOST = '0.0.0.0'  # Escucha en todas las interfaces disponibles
@@ -14,12 +16,66 @@ db_config = {
 }
 
 db = PostgresqlDatabase(**db_config)
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((HOST, PORT))
+        s.listen()
+        while True:
+            input("Press Enter to continue...")
+            # ask to the table Config of database db using peewee by id
+            # ask to the table Config of database db using peewee by id
+            config = Config.get_by_id(1)
+            # if fisical layer = 1 it means we need to use wifi, else we use BLE
+            print("Fisical layer: " + str(config.Fisical_Layer))
+            # id of the protocol to generate the data
+            print("Protocol: " + str(config.ID_protocol))
+            # transport layer to use (0 for ycp and 1 for tcp, in BLE 0 is for continue and 1 for sleep)
+            print("Transport layer: " + str(config.Transport_Layer))
+            print("El servidor está esperando conexiones en el puerto", PORT)
 
-while True:
-    input("Press Enter to continue...")
-    # ask to the table Config of database db using peewee by id
-    config = Config.get_by_id(1)
-    print(config.Fisical_Layer)
-    print(config.ID_protocol)
-    print(config.Transport_Layer)
-    
+            conn, addr = s.accept()
+            with conn:
+                print('Conectado por', addr)
+                conn.sendall((str(config.Fisical_Layer)+str(config.ID_protocol)+str(config.Transport_Layer)).encode('utf-8'))
+                # if fisical layer is 1 it means we need to use wifi, else we use BLE
+                if config.Fisical_Layer == 1:
+                     if config.protocol == 0 and config.Transport_Layer == 0:
+                        # use the protocol 0 using tcp
+                        print("\nPrepare to receive data from Protocol 0")
+                        data = conn.recv(1024)
+                        print("Recibido los bytes")
+                        # save data in the database
+                        print("Guardando en la base de datos")
+                        
+                        #get the first 2 bytes from the data and transform into a str id_device
+                        id_device = str(data[0:2].decode('utf-8'))
+                        print("ID: " + id_device)
+
+                        #get the next 6 bytes from the data and transform into a str MAC
+                        mac = ""
+                        for i in range(4):
+                            mac += bytes.hex(data[2+i:3+i]) + ":"
+                        mac = mac[:-1]
+                        print("MAC: " + mac)
+
+                        #get the next byte to know the transport layer
+                        transport_layer = chr(data[8])
+                        print("Transport layer: " + transport_layer)
+
+                        #get the next byte to get the protocol
+                        protocol = chr(data[9])
+                        print("Protocol: " + protocol)
+
+                        #get the next 2 bytes to get the packet length
+                        packet_length = int.from_bytes(data[10:12], signed=False, byteorder='little')
+                        print("Packet length: " + str(packet_length))
+
+                        #get the final byte with the battery level
+                        battery_level = data[12]
+                        print("Battery level: " + str(battery_level))
+
+                        #save the data in the database
+                        #Config.insert(ID_protocol='0', Transport_Layer='0').execute()
+                        Datos.insert(Id_device=id_device, MAC=mac, battlevel=battery_level).execute()
+                        Logs.insert(ID_device=id_device, Transport_Layer=transport_layer, finaltime=datetime.datetime.now()).execute()
+
+            
